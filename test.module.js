33c9ce4902d574b89bb15129a0f272ca8b92ba10
johnlenon <johnlenonmaghanoy@gmail.com>
2017-09-13 17:00:53 +0800

    Added tests.

--- test.module.js
+++ test.module.js
@@ -45,13 +45,13 @@
 
 	@include:
 		{
-			"assert": "should",
+			"assert": "should/as-function",
 			"statis": "statis"
 		}
 	@end-include
 */
 
-const assert = require( "should" );
+const assert = require( "should/as-function" );
 
 //: @server:
 const statis = require( "./statis.js" );
@@ -67,27 +67,116 @@ const path = require( "path" );
 
 
 //: @server:
-
 describe( "statis", ( ) => {
 
-} );
+	describe( "`statis with blueprint`", ( ) => {
+		it( "should chain static attachment", ( ) => {
+			let Test = statis( function Test( ){ } )
+			.attach( "hello", "world" )
+			.attach( 123, "yeah" )
+			.bind( {
+				"test": "test123"
+			} )
+			.implement( "testing", function testing( ){
+				return "test value";
+			} )
+			.merge( [ 1, 2, 3 ] )
+			.eject( );
 
+
+			assert.equal( Test.hello, "world" );
+
+			assert.equal( Test[ 123 ], "yeah" );
+
+			assert.equal( Test.test, "test123" );
+
+			assert.equal( typeof Test.testing, "function" );
+
+			assert.equal( Test.testing( ), "test value" );
+
+			assert.deepEqual( Test.slice( ), [ 1, 2, 3 ] );
+		} );
+	} );
+
+} );
 //: @end-server
 
 
 //: @client:
-
 describe( "statis", ( ) => {
 
-} );
+	describe( "`statis with blueprint`", ( ) => {
+		it( "should chain static attachment", ( ) => {
+			let Test = statis( function Test( ){ } )
+			.attach( "hello", "world" )
+			.attach( 123, "yeah" )
+			.bind( {
+				"test": "test123"
+			} )
+			.implement( "testing", function testing( ){
+				return "test value";
+			} )
+			.merge( [ 1, 2, 3 ] )
+			.eject( );
 
+
+			assert.equal( Test.hello, "world" );
+
+			assert.equal( Test[ 123 ], "yeah" );
+
+			assert.equal( Test.test, "test123" );
+
+			assert.equal( typeof Test.testing, "function" );
+
+			assert.equal( Test.testing( ), "test value" );
+
+			assert.deepEqual( Test.slice( ), [ 1, 2, 3 ] );
+		} );
+	} );
+
+} );
 //: @end-client
 
 
 //: @bridge:
-
 describe( "statis", ( ) => {
 
-} );
+	let bridgeURL = `file://${ path.resolve( __dirname, "bridge.html" ) }`;
 
+	describe( "`statis with blueprint`", ( ) => {
+		it( "should chain static attachment", ( ) => {
+			//: @ignore:
+			let result = browser.url( bridgeURL ).execute(
+
+				function( ){
+					let Test = statis( function Test( ){ } )
+					.attach( "hello", "world" )
+					.attach( 123, "yeah" )
+					.bind( {
+						"test": "test123"
+					} )
+					.implement( "testing", function testing( ){
+						return "test value";
+					} )
+					.merge( [ 1, 2, 3 ] )
+					.eject( );
+
+					return Test.hello == "world" &&
+						Test[ 123 ] == "yeah" &&
+						Test.test == "test123" &&
+						typeof Test.testing == "function" &&
+						Test.testing( ) == "test value" &&
+						Test.slice( )[ 0 ] == 1 &&
+						Test.slice( )[ 1 ] == 2 &&
+						Test.slice( )[ 2 ] == 3;
+				}
+
+			).value;
+			//: @end-ignore
+
+			assert.equal( result, true );
+		} );
+	} );
+
+} );
 //: @end-bridge
